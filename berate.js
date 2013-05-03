37c7a17e43a2a8665445580398bd6d82c3ea7594
John Fawcett <jrf0110@gmail.com>
2013-05-02 21:28:56 -0500

    get insults from insults server

--- berate.js
+++ berate.js
@@ -2,7 +2,7 @@
 //   Berate somebody
 //
 // Dependencies:
-//   None
+//   request: ~2.21.0
 //
 // Configuration:
 //   None
@@ -13,21 +13,42 @@
 // Author:
 //   John Fawctt
 
-var insults = [
+var request = require('request');
+
+var canned = [
   "{name}, you're just a stupid penis-gobbler."
 , "I used to talk to {name}, but then I stopped talking to dumbasses."
 ];
+
+var getInsults = function(callback){
+  var options = {
+    url: 'http://insults.j0.hn/insults?limit=10000'
+  , json: true
+  };
+
+  request.get(options, function(error, response, body){
+    if (error) return callback(error);
+
+    if (response.statusCode != 200) return callback(true);
+
+    callback(null, body);
+  });
+};
 
 module.exports = function(robot) {
   robot.respond(/(berate|insult) (.*)/i, function(msg){
     if (msg.match.length < 2) return;
 
-    var name = msg.match.slice(-1)[0];
+    getInults(function(error, insults){
+      if (error) insults = canned;
 
-    msg.reply(
-      insults[
-        parseInt(Math.random() * insults.length)
-      ].replace(/{name}/g, name)
-    );
+      var name = msg.match.slice(-1)[0];
+
+      msg.reply(
+        insults[
+          parseInt(Math.random() * insults.length)
+        ].replace(/{name}/g, name)
+      );
+    });
   });
 }
